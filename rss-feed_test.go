2bf7b1ee7a802a17951141fbe96f7c810ad4106e
Vichar Nuchsiri 52545 <vichar.nuchsiri@fico.com>
2019-02-19 17:17:23 +0700

    Initial Commit

---
+++ rss-feed_test.go
@@ -0,0 +1,18 @@
+package rss-feed_test
+
+import (
+	"testing"
+
+	"github.com/vichar/rss-feed"
+)
+
+func Test1(t *testing.T) {
+	// t.Run("1 should say 1", func(t *testing.T) {
+	// 	get := fizzbuzz.Say(1)
+	// 	want := "1"
+	// 	if want != get {
+	// 		t.Errorf("It should say %s but get %s", want, get)
+	// 	}
+	// })
+
+}

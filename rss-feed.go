878691d0c30c200969363a2a990f4e72558f52eb
Vichar Nuchsiri 52545 <vichar.nuchsiri@fico.com>
2019-02-20 16:16:21 +0700

    adding rss Parser

--- rss-feed.go
+++ rss-feed.go
@@ -1,18 +1,66 @@
 package rssfeed
 
 import (
+	"container/list"
 	"errors"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+
+	"github.com/mmcdole/gofeed"
 )
 
-func main() {}
+// ParseRSSData Parsing RSS Data from String
+func ParseRSSData(xmlData string) (RSSChannel, error) {
+	fp := gofeed.NewParser()
+	feed, error := fp.ParseString(xmlData)
+	var channel RSSChannel
+	if error == nil {
+		channel.Title = feed.Title
+		channel.Link = feed.Link
+		channel.Description = feed.Description
+		channel.Language = feed.Language
+		channel.LastBuildDate = feed.PublishedParsed
+		channel.ImageURL = feed.Image.URL
+		var items = list.New()
+		for _, item := range feed.Items {
+			var rssItem RSSItem
+			rssItem.Title = item.Title
+			rssItem.Description = item.Description
+			rssItem.MediaLink = item.Enclosures[0].URL
+			rssItem.PubDate = item.PublishedParsed
+			rssItem.Language = channel.Language
+			items.PushFront(rssItem)
+		}
+		channel.Items = items
+		return channel, nil
+	}
+	return channel, errors.New("Invalid RSS Feed")
+}
 
-//GetContent is function to retrieve Content of an rss feed
-func GetContent(feedURL string) (*http.Response, error) {
+//ParseHTTPResponse Parse RSS Content from HTTP Response Body
+func ParseHTTPResponse(response *http.Response) HTTPResponse {
+	var responseObject HTTPResponse
+	responseObject.HTTPStatus = response.Status
+	responseObject.HTTPStatusCode = response.StatusCode
+	if response.StatusCode == http.StatusOK {
+		defer response.Body.Close()
+		bodyBytes, _ := ioutil.ReadAll(response.Body)
+		bodyString := string(bodyBytes)
+		responseObject.Body = bodyString
+
+		return responseObject
+	}
+	responseObject.Body = ""
+	return responseObject
+}
+
+//HTTPGet is a function to retrieve Content of an rss feed
+func HTTPGet(feedURL string) (*http.Response, error) {
 	r := validateURL(feedURL)
 	if r {
-		return http.Get(feedURL)
+		response, error := http.Get(feedURL)
+		return response, error
 	}
 
 	return nil, errors.New("Invalid URL")
--- rss-feed_test.go
+++ rss-feed_test.go
@@ -1,17 +1,61 @@
 package rssfeed_test
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/vichar/rssfeed"
 )
 
-func TestGetContentWithValidURL(t *testing.T) {
-	t.Run("GetContent should return valid String URL", func(t *testing.T) {
-		response, error := rssfeed.GetContent("https://www.npr.org")
+func TestParseRSSDataWithValidRSSURL(t *testing.T) {
+
+	t.Run("ParseHTTPResponse", func(t *testing.T) {
+		getResponse, httpError := rssfeed.HTTPGet("https://www.npr.org/rss/podcast.php?id=500005")
+		if httpError == nil {
+			response := rssfeed.ParseHTTPResponse(getResponse)
+			rssChannel, parsingError := rssfeed.ParseRSSData(response.Body)
+			if parsingError == nil {
+				if rssChannel.Items.Len() <= 0 {
+					t.Errorf("Parsed Feed should contain at least 1 item")
+				}
+				if len(rssChannel.Title) <= 0 {
+					t.Errorf("Parsed Feed should contain title")
+
+				}
+			} else {
+				t.Errorf("ParseRSSData should not return error for a valid URL but gets %s", parsingError.Error())
+			}
+		} else {
+			t.Errorf("ParseHTTPResponse should not return error for a valid URL but gets %s", httpError.Error())
+		}
+	})
+}
+func TestParseHTTPResponseWithValidURL(t *testing.T) {
+	t.Run("ParseHTTPResponse", func(t *testing.T) {
+		response, error := rssfeed.HTTPGet("https://www.npr.org/rss/podcast.php?id=500005")
+		if error == nil {
+			var getResponse rssfeed.HTTPResponse = rssfeed.ParseHTTPResponse(response)
+			if getResponse.HTTPStatus != "200 OK" {
+				t.Errorf("HTTP Status shoud be %s but gets %s", getResponse.HTTPStatus, "200 OK")
+			}
+			if getResponse.HTTPStatusCode != 200 {
+				t.Errorf("HTTP Status Code should be %s but gets %s", strconv.Itoa(getResponse.HTTPStatusCode), "200")
+			}
+			if len(getResponse.Body) <= 0 {
+				t.Errorf("HTTP Response Body should not be empty but length is %s", strconv.Itoa(len(getResponse.Body)))
+			}
+		} else {
+			t.Errorf("HTTPGet should not return error for a valid URL but gets %s", error.Error())
+		}
+	})
+}
+
+func TestHTTPGetWithValidURL(t *testing.T) {
+	t.Run("HTTPGet should return valid String URL", func(t *testing.T) {
+		response, error := rssfeed.HTTPGet("https://www.npr.org/rss/podcast.php?id=500005")
 		want := "200 OK"
 		if want != response.Status {
-			t.Errorf("It should say %s but get %s", want, response.Status)
+			t.Errorf("It should say %s but gets %s", want, response.Status)
 		}
 		if error != nil {
 			t.Errorf("An Unexpected Error occurs %s", error.Error())
@@ -22,10 +66,13 @@ func TestGetContentWithValidURL(t *testing.T) {
 }
 
 func TestGetContentWithInvalidURL(t *testing.T) {
-	t.Run("GetContent should return Empty String", func(t *testing.T) {
-		response, error := rssfeed.GetContent("")
+	t.Run("HTTPGet should return Empty String", func(t *testing.T) {
+		response, error := rssfeed.HTTPGet("")
 		if error == nil {
-			t.Errorf("An Error is Expected %s %s", response.Status, error.Error())
+			t.Errorf("Invalid URL is Expected %s %s", response.Status, error.Error())
+		}
+		if error.Error() != "Invalid URL" {
+			t.Errorf("Unexpected Error has occurred %s %s", response.Status, error.Error())
 		}
 	})
 
